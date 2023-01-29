4520b786140684a421c9e06abbb57c031253e131
Gildo Neto <gildo.neto@telus.com>
2023-01-29 09:18:11 -0300

    60. stateless vs stateful components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated');
-  };
 
   return (
   <Card className='expense-item'>
@@ -18,10 +10,9 @@ const ExpenseItem = (props) => {
       date={props.date}
     />
     <div className='expense-item__description'>
-      <h2>{title}</h2>
+      <h2>{props.title}</h2>
       <div className='expense-item__price'>R${props.amount}</div>
     </div>
-    <button onClick={clickHandler}>Change title</button>
   </Card>
     )
 }
