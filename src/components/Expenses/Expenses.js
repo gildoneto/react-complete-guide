ae7c03667aff3f3026c7aa50ae53203158457c54
Gildo Neto <gildo.neto@telus.com>
2023-01-29 09:51:58 -0300

    64. using stateful lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,23 +5,11 @@ import './Expenses.css'
 import { useState } from 'react';
 
 const Expenses = (props) => {
-  const [items, setItems] = useState(props.items);
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   }
-
-  const filteredItems = items.filter((item) => item.date.getFullYear().toString()  === filteredYear);
-
-  const expenseItems = filteredItems.map((expense, index) =>
-    <ExpenseItem
-      key={index}
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-    />
-  );
 
   return (
     <Card className='expenses'>
@@ -29,7 +17,17 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expenseItems}
+      {
+        props.items.map((expense, index) => (
+          <ExpenseItem
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+          )
+        )
+      }
     </Card>
   );
 
