f5818ac3690a0f46393fc5c3027a51adeea972ef
Gildo Neto <gildo.neto@telus.com>
2023-01-29 08:18:47 -0300

    assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,34 @@
 import ExpenseItem from './ExpenseItem'
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css'
+import { useState } from 'react';
 
 const Expenses = (props) => {
-  const items = props.items.map((expense, index) =>
-  <ExpenseItem
-    key={index}
-    title={expense.title}
-    amount={expense.amount}
-    date={expense.date}
-  />)
+  const [items, setItems] = useState(props.items);
+  const [filterYear, setFilterYear] = useState('');
 
-  return <Card className='expenses'>{items}</Card>;
+  const onChangeYearHandler = (inputYear) => {
+    setFilterYear(inputYear);
+  }
+
+  const filteredItems = items.filter((item) => item.date.getFullYear().toString()  === filterYear);
+
+  const expenseItems = filteredItems.map((expense, index) =>
+    <ExpenseItem
+      key={index}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  );
+
+  return (
+    <Card className='expenses'>
+      <ExpensesFilter onChangeYear={onChangeYearHandler}/>
+      {expenseItems}
+    </Card>
+  );
 
 }
 
