383f3fa4ec9f273eabde9fa93dfe4bc6f3c9f137
Gildo Neto <gildo.neto@telus.com>
2023-02-03 16:54:45 -0300

    93 adding validation & resetting logic

--- 04-starting-project/src/components/Users/AddUser.js
+++ 04-starting-project/src/components/Users/AddUser.js
@@ -9,7 +9,15 @@ const AddUser = props => {
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    console.log(enteredUsername, enteredAge);
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge)
+    setEnteredAge('');
+    setEnteredUsername('');
   };
 
   const usernameChangeHandler = (e) => {
@@ -24,9 +32,19 @@ const AddUser = props => {
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler}/>
+        <input 
+          id="username" 
+          type="text" 
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="username">Age (Years)</label>
-        <input id="username" type="number" onChange={ageChangeHandler}/>
+        <input 
+        id="username" 
+        type="number" 
+        value={enteredAge}
+        onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
