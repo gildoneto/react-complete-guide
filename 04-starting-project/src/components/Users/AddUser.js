92f034e6bf2c9be5bec724c95ff217e8c66da68e
Gildo Neto <gildo.neto@telus.com>
2023-02-03 16:17:02 -0300

    90 re-usable "Card" Component

---
+++ 04-starting-project/src/components/Users/AddUser.js
@@ -0,0 +1,22 @@
+import Card from '../UI/Card';
+import styles from './AddUser.module.css';
+
+const AddUser = props => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+
+  }
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="username">Age (Years)</label>
+        <input id="username" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
