29cc1696bfed3e4203374b3612ee35b332ed8137
Gildo Neto <gildo.neto@thoughtworks.com>
2023-03-04 01:20:54 -0300

    124. Building & Using a Custom Context Provider Component

--- 05-login-side-effects/src/components/Login/Login.js
+++ 05-login-side-effects/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -24,23 +25,21 @@ const passwordReducer = (state, action) => {
   return { value: '', isValid: false };
 };
 
-const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
+const Login = props => {
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const [emailState, dispatchEmail] = useReducer(emailReducer, { 
-    value: '', 
-    isValid: null 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
   });
-  
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, { 
-    value: '', 
-    isValid: null 
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '',
+    isValid: null,
   });
-  
+
+  const authCtx = useContext(AuthContext);
+
   /*
     useEffect com array de dependências vazio
     só roda a primeira vez que o componente renderiza.
@@ -52,7 +51,7 @@ const Login = (props) => {
     console.log('EFFECT RUNNING');
 
     return () => {
-      console.log('EFFECT CLEANUP')
+      console.log('EFFECT CLEANUP');
     };
   }, []);
 
@@ -70,9 +69,7 @@ const Login = (props) => {
 
     const identifier = setTimeout(() => {
       console.log('Checking validity');
-      setFormIsValid(
-        emailIsValid && passwordIsValid
-      );
+      setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
     return () => {
       console.log('CLeanUP');
@@ -80,20 +77,12 @@ const Login = (props) => {
     };
   }, [emailIsValid, passwordIsValid]);
 
-  const emailChangeHandler = (event) => {
-    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
-
-    // setFormIsValid(
-    //   event.target.value.includes('@') && passwordState.isValid
-    // );
+  const emailChangeHandler = event => {
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
   };
 
-  const passwordChangeHandler = (event) => {
+  const passwordChangeHandler = event => {
     dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
-
-    // setFormIsValid(
-    //   emailState.isValid && event.target.value.trim().length > 6
-    // );
   };
 
   const validateEmailHandler = () => {
@@ -104,9 +93,9 @@ const Login = (props) => {
     dispatchPassword({ type: 'INPUT_BLUR' });
   };
 
-  const submitHandler = (event) => {
+  const submitHandler = event => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
