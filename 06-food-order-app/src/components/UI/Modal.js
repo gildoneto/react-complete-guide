04f20924fe0899a7382e75450b6ffaa249ae15f6
Gildo Neto <gildoneto@hotmail.com>
2023-06-17 14:33:20 -0300

    173. Adding a Modal via React Portal

---
+++ 06-food-order-app/src/components/UI/Modal.js
@@ -0,0 +1,29 @@
+import ReactDOM from 'react-dom';
+import styles from './Modal.module.css';
+
+const Backdrop = () => {
+  return <div className={styles.backdrop} />;
+};
+const ModalOverlay = ({ children }) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = ({ children }) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
