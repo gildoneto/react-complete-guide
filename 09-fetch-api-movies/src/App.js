5397e910de8abed322cfde44018d7a86aa2621ef
Gildo Neto <gildoneto@hotmail.com>
2023-06-20 23:33:43 -0300

    216. Sending a POST Request

--- 09-fetch-api-movies/src/App.js
+++ 09-fetch-api-movies/src/App.js
@@ -4,8 +4,8 @@ import MoviesList from './components/MoviesList';
 import AddMovie from './components/AddMovie';
 import './App.css';
 
-const baseUrl = 'https://swapi.dev/';
-const baseUrlBackup = 'https://swapi.py4e.com/';
+// const baseUrl = 'https://swapi.dev/';
+// const baseUrlBackup = 'https://swapi.py4e.com/';
 const firebaseUrl = '';
 
 function App() {
@@ -24,15 +24,19 @@ function App() {
       }
       
       const data = await response.json();
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseData: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
+
+      const loadedMovies = [];
+
+      for (const key in data ) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -43,8 +47,17 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(firebaseUrl + '/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>
