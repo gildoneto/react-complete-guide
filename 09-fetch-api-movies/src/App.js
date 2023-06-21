2abf3646c66bcc0b8d72d3fb1e55110393103461
Gildo Neto <gildoneto@hotmail.com>
2023-06-20 23:06:13 -0300

    214. Using useEffect() For Requests

--- 09-fetch-api-movies/src/App.js
+++ 09-fetch-api-movies/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -11,11 +11,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback( async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(baseUrlBackup + 'api/films/');
+      const response = await fetch(baseUrl + 'api/films/');
       
       if (!response.ok) {
         throw new Error('Something went wrong!');
@@ -35,7 +35,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>
 
