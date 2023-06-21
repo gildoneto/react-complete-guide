2c6634234845413d728980da578635c3d46d4f38
Gildo Neto <gildoneto@hotmail.com>
2023-06-20 21:53:38 -0300

    Finalizing Class-based Components

--- 08-toggle-users-class-components/src/components/UserFinder.js
+++ 08-toggle-users-class-components/src/components/UserFinder.js
@@ -1,14 +1,52 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, Component } from 'react';
 
 import Users from './Users';
+import UsersContext from '../store/users-context';
 import classes from './UserFinder.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+class UserFinder extends Component {
+  static contextType = UsersContext;
 
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: '',
+    }
+  }
+
+  componentDidMount() {
+    // Send http request...
+    this.setState({ filteredUsers: this.context.users});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+/*
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
@@ -32,5 +70,6 @@ const UserFinder = () => {
     </Fragment>
   );
 };
+*/
 
 export default UserFinder;
