4e99acd9a28f08fa6710d94c14ed0036628a8b93
Gildo Neto <gildoneto@hotmail.com>
2023-06-26 15:37:50 -0300

    251. Handling Errors

--- 12-food-order-api/src/components/Meals/AvailableMeals.js
+++ 12-food-order-api/src/components/Meals/AvailableMeals.js
@@ -8,11 +8,16 @@ const firebaseUrl = process.env.REACT_APP_API_URL;
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
 
     const fetchMeals = async () => {
       const response = await fetch(firebaseUrl + '/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -30,8 +35,13 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
-  }, [])
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+    
+  }, []);
+
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
@@ -46,6 +56,12 @@ const AvailableMeals = () => {
     return <section className={classes.MealsLoading}>
       <p>Loading...</p>
     </section>
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>
+    <p>{httpError}</p>
+  </section>
   }
 
   return (
