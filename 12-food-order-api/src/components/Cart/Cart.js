da84faf7241280665bf109e3b82203819d02b43e
Gildo Neto <gildoneto@hotmail.com>
2023-06-27 22:06:33 -0300

    255. Submitting & Sending Cart Data

--- 12-food-order-api/src/components/Cart/Cart.js
+++ 12-food-order-api/src/components/Cart/Cart.js
@@ -6,6 +6,8 @@ import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 import Checkout from './Checkout';
 
+const firebaseUrl = process.env.REACT_APP_API_URL;
+
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
 
@@ -24,6 +26,16 @@ const Cart = (props) => {
 
   const orderHandler = () => {
     setIsCheckout(true);
+  };
+
+  const submitOrderHandler = (userData) => {
+    fetch(`${firebaseUrl}/orders.json`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    });
   };
 
   const cartItems = (
@@ -61,7 +73,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose}/>}
+      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>}
       {!isCheckout && modalActions}
     </Modal>
   );
