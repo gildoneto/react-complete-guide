99b189725426d5ac57adb9410ede02f1a3cb7551
Gildo Neto <gildoneto@hotmail.com>
2023-07-03 23:38:00 -0300

    265. More Redux Basics

--- 13a-redux-demo/redux-demo.js
+++ 13a-redux-demo/redux-demo.js
@@ -1,10 +1,22 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 };
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
 
 const store = redux.createStore(counterReducer);
 
@@ -16,3 +28,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
