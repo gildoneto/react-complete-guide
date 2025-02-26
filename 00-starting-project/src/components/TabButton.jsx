25468e4f177a58e4b0237287a3ae9508e4bb909a
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2025-02-26 10:18:20 -0300

    Enhance TabButton component to indicate selected state and update App to handle unselected topic scenario

--- 00-starting-project/src/components/TabButton.jsx
+++ 00-starting-project/src/components/TabButton.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
     <li>
       <button
+      className={isSelected ? 'active' : ''}
         onClick={onSelect}
       >
         {children}
