d0ac7234d76ec232f42311fb92f20f46aa53e86c
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2025-02-26 09:54:37 -0300

    Update App component to display example content based on selected topic

--- 00-starting-project/src/App.jsx
+++ 00-starting-project/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
 
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -49,7 +49,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-            {selectedTopic}
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
         </section>
       </main>
     </div>
