514ff94ed35a3e6f75f92e71aef014f130674fc7
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2025-02-26 09:22:58 -0300

    Add TabButton component and integrate it into App for navigation

--- 00-starting-project/src/App.jsx
+++ 00-starting-project/src/App.jsx
@@ -1,8 +1,14 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  let tabContent = 'Please click a button';
+
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -31,6 +37,16 @@ function App() {
               description={CORE_CONCEPTS[3].description}
             />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+            {tabContent}
         </section>
       </main>
     </div>
