94af1b869d34474e880d28b2f55d3bf657f2d2e9
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2025-02-26 09:38:24 -0300

    Refactor App component to manage selected topic state and update TabButton functionality

--- 00-starting-project/src/App.jsx
+++ 00-starting-project/src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  let tabContent = 'Please click a button';
+
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
 
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
   }
   return (
     <div>
@@ -46,7 +49,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-            {tabContent}
+            {selectedTopic}
         </section>
       </main>
     </div>
--- 00-starting-project/src/components/TabButton.jsx
+++ 00-starting-project/src/components/TabButton.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable react/prop-types */
 export default function TabButton({ children, onSelect }) {
-
   return (
     <li>
       <button
